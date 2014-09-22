3dbcbe185ab2cd58f3e4712d07cfbef8ad4a65ef
Chang-Hung Liang <eliang.cs@gmail.com>
2014-09-22 20:38:32 +0800

    Add version and export some classes

--- happybase_mock/__init__.py
+++ happybase_mock/__init__.py
@@ -1,1 +1,8 @@
-from .connection import Connection  # noqa
+# flake8: noqa
+
+__version__ = '0.8.0'
+
+from .batch import Batch
+from .connection import Connection, DEFAULT_HOST, DEFAULT_PORT
+from .pool import ConnectionPool
+from .table import Table
